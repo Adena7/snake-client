024d2f682a24f72ce2927ea45115ddb8b04c2d95
Abdoullahi Aden <abdoullahi.aden@gmail.com>
2021-12-02 00:24:35 +0000

    Separate Module For The TCP Connection

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+let IP = '165.227.47.243';
+const PORT = 50541;
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: IP, // IP address here,
+    port: PORT // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: AAA")
+})
+// interpret incoming data as text
+conn.on("data", (data) => {
+    console.log(data)
+})
+  return conn;
+};
+
+module.exports = { connect };
